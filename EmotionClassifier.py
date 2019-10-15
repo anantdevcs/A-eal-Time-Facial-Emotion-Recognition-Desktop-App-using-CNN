cd07277be7143697fc71bbaebde30b9f3d677f1c
anantdevcs <anantdeviitism@gmail.com>
2019-10-15 20:54:02 +0530

    Minor UI improvements

--- EmotionClassifier.py
+++ EmotionClassifier.py
@@ -9,6 +9,7 @@ from Webcam import Webcam as WC
 from tkinter import *
 from tkinter import font
 import cv2
+import tkinter
 from PIL import Image, ImageTk
 #from tkinter.ttk import *
 
@@ -26,8 +27,13 @@ class Welcome:
         Webcam = WC(top)
     def __init__(self, master):
         self.master = master
-        master.title('Welcome')
+        master.title('Face Emotion App')
         master.geometry("400x400")
+        image=Image.open('FacePic.png')
+        image = image.resize((250, 250), Image.ANTIALIAS) ## The (250, 250) is (height, width)
+        self.bgimg = ImageTk.PhotoImage(image)
+        self.panel = Label(master, image = self.bgimg)
+        self.panel.pack(side = "bottom")
         helv36 = font.Font(family='Lucida Sans', size=36, weight='bold')
         self.label = Label(master, text="Welcome!",font=helv36,anchor="center")
         self.label.config(anchor=CENTER)
@@ -35,10 +41,10 @@ class Welcome:
         helv1 = font.Font(family='Lucida Sans', size=18, weight='bold')
         self.greet_button = Button(master, text="Start!", command=self.run_webam,font=helv1)
         self.greet_button.config(anchor=CENTER)
-        self.greet_button.pack()
+        self.greet_button.pack(side="top")
         self.close_button = Button(master, text="Close", command=master.quit ,font=helv1)
         self.close_button.config(anchor=CENTER)
-        self.close_button.pack()
+        self.close_button.pack(side="bottom")
         
 
 #root = Tk()
@@ -52,10 +58,19 @@ class Welcome:
 def run():
     root = Tk()
     center(root)
-    Welcome_window = Welcome(root)
+    Wel = Welcome(root)
     root.mainloop()
     
+# def run():
+#     root = tk.ThemedTk()
+
+#     root.get_themes()                 # Returns a list of all themes that can be set
+#     root.set_theme("radiance")
+
+    
 run()
+# style = ttk.Style()
+# style.configure("BW.TLabel", foreground="black", background="white")
 
 
 
