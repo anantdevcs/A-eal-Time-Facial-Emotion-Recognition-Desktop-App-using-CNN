685038f669a44d1438829e346f3d37e0d773e9ae
anantdevcs <anantdeviitism@gmail.com>
2019-10-15 13:10:23 +0530

    Added the functionality of image segmentation for face

--- Webcam.py
+++ Webcam.py
@@ -1,6 +1,7 @@
 from tkinter import *
 import cv2
 from PIL import Image, ImageTk
+import tensorflow as tf
 
 class Webcam:
     def __init__(self, master):
@@ -11,6 +12,18 @@ class Webcam:
         self.lmain.config(anchor=CENTER)
         self.lmain.pack()
         self.setup()
+    def detect_faces(self , img):
+        face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml') 
+        gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+        faces = face_cascade.detectMultiScale(gray, 1.3, 5) 
+        
+        for (x,y,w,h) in faces: 
+            # To draw a rectangle in a face  
+            cv2.rectangle(img,(x,y),(x+w,y+h),(255,255,0),2)  
+#            roi_color = img[y:y+h, x:x+w] 
+        return img
+    
+        
     def setup(self):
         width, height = 800, 600
         self.cap = cv2.VideoCapture(0)
@@ -22,21 +35,14 @@ class Webcam:
         _, frame = self.cap.read()
         frame = cv2.flip(frame, 1)
         cv2image = cv2.cvtColor(frame, cv2.COLOR_BGR2RGBA)
+        cv2image=self.detect_faces(cv2image)
         img = Image.fromarray(cv2image)
         imgtk = ImageTk.PhotoImage(image=img)
         self.lmain.imgtk = imgtk
         self.lmain.configure(image=imgtk)
         self.lmain.after(200, self.read_faces)
         
-    def detect_faces(self , img):
-        face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml') 
-        gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-        faces = face_cascade.detectMultiScale(gray, 1.3, 5) 
-        for (x,y,w,h) in faces: 
-            # To draw a rectangle in a face  
-            cv2.rectangle(img,(x,y),(x+w,y+h),(255,255,0),2)  
-            roi_gray = gray[y:y+h, x:x+w] 
-            roi_color = img[y:y+h, x:x+w] 
+    
 
         
         
